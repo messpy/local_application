5cac7531485ac4931e668216c390ee9b98b20aa0
messsykenty <110882522+messpy@users.noreply.github.com>
2024-03-09 10:06:12 +0900

    Update 翻訳.py

--- 翻訳.py
+++ 翻訳.py
@@ -3,19 +3,6 @@ from turtle import width
 from googletrans import Translator
 import tkinter as tk
 import win32com.client as wincl
-import winsound
-
-
-winsound.Beep(262, 500)  # ド（262Hzの音を1000msec流す）
-winsound.Beep(294, 500)  # レ（294Hzの音を1000msec流す）
-winsound.Beep(330, 500)  # ミ（330Hzの音を1000msec流す）
-winsound.Beep(349, 500)  # ファ（349Hzの音を1000msec流す）
-winsound.Beep(392, 500)  # ソ（392Hzの音を1000msec流す）
-winsound.Beep(440, 500)  # ラ（440Hzの音を1000msec流す）
-winsound.Beep(494, 500)  # シ（494Hzの音を1000msec流す）
-
-for i in range(1):  # 5回繰り返す
-    winsound.Beep(523, 900)
 
 
 def key_event(e):
