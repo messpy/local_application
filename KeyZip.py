086bf7065b9d60128070cb9fb3ebba41c8c1d579
messsykenty <110882522+messpy@users.noreply.github.com>
2024-03-09 09:51:14 +0900

    Update KeyZip.py

--- KeyZip.py
+++ KeyZip.py
@@ -1,35 +1,13 @@
-import zipfile
-from tkinter import filedialog
+import itertools
 
+# A-zと0-9のリスト
+alphabet = list("abcdefghijklmnopqrstuvwxyz")
+numbers = list("0123456789")
 
-zff = filedialog.askopenfilename(title="zip")
-
-def openzip(pas,f):
-    zff = f
-    zfff=r"C:\Users\username\Desktop"
-    with zipfile.ZipFile(zff,"r")as zf:
-        try:
-            zf.extractall(path=zfff,pwd=pas.encode())
-            print("Password:",pas)
-            input("終了")
-        except Exception as e:
-            pass
-        
-def main(f):
-    num = [97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122]
-    num1 = [48,49,50,51,52,53,54,55,56,57]
-    mypass = []
-
-    for a in num:
-        for b in num:
-            for c in num:
-                for d in num:
-                    yp = chr(a) + chr(b) + chr(c) + chr(d)
-                    print(yp)
-
-                    openzip(yp,f)
-
-
-                    
-if __name__ == "__main__":
-    main(zff)
+# itertools.combinations_with_replacementを使って、8文字までのすべての組み合わせを生成
+for i in range(1,8):
+    print(str(i)+"*"*10)
+    combinations = itertools.combinations_with_replacement(alphabet + numbers,i)
+    # 組み合わせをすべて表示
+    for combination in combinations:
+        print("".join(combination))
