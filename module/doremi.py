80e797e64e31013befdf285954dab62f977ca7e8
messsykenty <110882522+messpy@users.noreply.github.com>
2024-03-09 10:06:56 +0900

    Create doremi.py

---
+++ module/doremi.py
@@ -0,0 +1,9 @@
+import winsound
+
+winsound.Beep(262, 500)  # ド（262Hzの音を1000msec流す）
+winsound.Beep(294, 500)  # レ（294Hzの音を1000msec流す）
+winsound.Beep(330, 500)  # ミ（330Hzの音を1000msec流す）
+winsound.Beep(349, 500)  # ファ（349Hzの音を1000msec流す）
+winsound.Beep(392, 500)  # ソ（392Hzの音を1000msec流す）
+winsound.Beep(440, 500)  # ラ（440Hzの音を1000msec流す）
+winsound.Beep(494, 500)  # シ（494Hzの音を1000msec流す）
