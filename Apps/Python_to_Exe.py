e070e0567e50ddc86b9fd9a6dfc8a84ee0e32455
messsykenty <110882522+messpy@users.noreply.github.com>
2024-03-09 10:28:22 +0900

    Update and rename Python_to_Exe.py to Apps/Python_to_Exe.py

--- Apps/Python_to_Exe.py
+++ Apps/Python_to_Exe.py
@@ -7,9 +7,6 @@ fTyp = [("exeにしたいpyFile", "*.py")]
 iDir = os.path.abspath(os.path.dirname(__file__))
 pyfile = tkinter.filedialog.askopenfilename(filetypes=fTyp, initialdir=iDir)
 
-#pgui.hotkey("Enter")
-
-
 result = subprocess.run(["cd", "desktop"], capture_output=True, encoding="utf-8")
 result = subprocess.run(["pyinstaller",pyfile, "--onefile"], capture_output=True, encoding="utf-8")
 print(result.stdout, end="")
